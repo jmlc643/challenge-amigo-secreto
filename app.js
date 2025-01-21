9105523c1342d38d32dc2d5ff0abe55044a19821
jmlc643 <josemarialuyocampos@gmail.com>
2025-01-21 11:05:37 -0500

    Add 'agregarAmigo' function

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+let inputAmigo = document.getElementById('amigo');
+
+function agregarAmigo() {
+    if (inputAmigo.value) {
+        const amigo = inputAmigo.value;
+        amigos.push(amigo);
+        console.log(amigos);
+        limpiarInputs();
+    } else {
+        alert("Por favor, inserte un nombre.");
+    }
+}
+
+function limpiarInputs() {
+    inputAmigo.value = "";
+}
